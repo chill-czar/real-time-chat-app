4f611828bc2dceb9f82ab6cbc264425f572898f9
aisarthak <aisarthak2@gmail.com>
2024-11-24 11:49:47 +0530

    pages routing setup

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,6 @@
 import Message from "../models/message.model.js";
 import User from "./../models/user.model.js";
-
+import cloudinary from "./../lib/cloudnairy.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.User._id;
@@ -32,4 +32,33 @@ export const getMessages = async (req, res) => {
   }
 };
 
-export const sendMessage = async (req, res) => {};
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // toddo: realtime functionality goes here =. sockeet io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMesssage controller:", error.message);
+    res.status(500).json({ error: "internal server error " });
+  }
+};
