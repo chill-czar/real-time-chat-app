e2c6d5c872939dd17d919091e99c29602eacbc7f
aisarthak <aisarthak2@gmail.com>
2024-11-24 17:04:23 +0530

    Remove unused dependencies and clean up related files

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
