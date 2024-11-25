722f97d3b65dce1d4df5bf8697e7185bf0cc615c
aisarthak <aisarthak2@gmail.com>
2024-11-25 19:43:59 +0530

    sfsdf

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,18 +5,27 @@ import { conneceDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import path from "path";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const __dirname = path.resolver();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if(process.env.NODE_ENV === "production"){
+  app.use(expres.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend" , "dist","index.html"));
+  })
+}
+
 
 server.listen(PORT, () => {
   console.log("Server is running on port" + PORT);
