820d3b5f51efa038e2221286edaa8c1d38fe5df4
aisarthak <aisarthak2@gmail.com>
2024-11-25 11:38:07 +0530

    Refactor Sidebar to use online users from auth store; initialize onlineUsers state in useAuthStore

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Sidebar = () => {
   const { getUsers, isUserLoading, users, selectedUser, setSelectedUser } =
     useChatStore();
-  const onlineUsers = [];
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getUsers();
