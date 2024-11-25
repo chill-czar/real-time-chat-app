4157e140f96d6ea73ff1f12d7c7c4a57b4f2c63e
aisarthak <aisarthak2@gmail.com>
2024-11-25 11:33:08 +0530

    #No Error : Fix user ID reference and improve error logging in getUsersForSidebar; enhance sidebar user display with online status

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -3,14 +3,15 @@ import User from "./../models/user.model.js";
 import cloudinary from "./../lib/cloudnairy.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
-    const loggedInUserId = req.User._id;
-    const fileteredUsers = await User.find({
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
-    res.status(200).json(fileteredUsers);
+
+    res.status(200).json(filteredUsers);
   } catch (error) {
-    console.log("Error in getUsersForSidebar", error.message);
-    res.status(500).json({ message: "Internal server error" });
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
